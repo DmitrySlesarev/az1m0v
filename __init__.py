0abb0efe1429bd1452e0d0562abe6c9bc72b7079
Dmitry Slesarev <dvslesar@gmail.com>
2025-11-16 11:47:48 -0300

    Update architecture status & draw architecture diagramm

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """EV project package."""
 
-__version__ = "0.0.0.1"
-__author__ = "EV Project Team"
-__description__ = "Electric Vehicle Management System"
+__version__ = "0.0.0.2"
+__author__ = "az1mov EV Project Team"
+__description__ = "az1mov Electric Vehicle Management System"
 
 __all__ = ["__version__", "__author__", "__description__"]
